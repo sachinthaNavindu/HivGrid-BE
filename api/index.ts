bd14af2cac3f403fcaf970d9c247f71c290fb4c3
Sachintha <navindus485@gmail.com>
2026-01-04 03:22:11 +0530

    added vercel

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
-import app from "../src/index"
+import app from "../src/index";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default app
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return app(req as any, res as any);
+}
