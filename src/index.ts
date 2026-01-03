d6163e1ffcdf778f576044cd96e72a1aa8669c91
Sachintha <navindus485@gmail.com>
2026-01-04 04:16:59 +0530

    final push

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: ["https://hiv-grid-fe.vercel.app"],
+    origin: ["https://hiv-grid-fe.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"]
   })
 );
