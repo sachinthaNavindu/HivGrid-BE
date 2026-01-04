8564079a737b7f60c33232f3cc305c3c5380c1ce
Sachintha <navindus485@gmail.com>
2026-01-04 05:41:39 +0530

    final push

--- src/index.ts
+++ src/index.ts
@@ -13,25 +13,17 @@ dotenv.config()
 const MONGO_URI = process.env.MONGO_URI as string
 
 const app = express()
+
 app.use(express.json())
 
 app.use(
   cors({
     origin: ["https://hiv-grid-fe.vercel.app"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Content-Length", "Authorization"]
+    methods: ["GET", "POST", "PUT", "DELETE"]
   })
-);
+)
 
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  console.log('Origin:', req.headers.origin);
-  console.log('Headers:', req.headers);
-  next();
-});
 
 app.use("/api/HivGrid/auth", authRouter)
 app.use("/api/HivGrid/home", homeRouter)
